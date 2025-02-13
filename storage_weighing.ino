220f67c277d6724d6c4410dbf54962605af60373
Spejb1 <160340105+Spejb1@users.noreply.github.com>
2025-02-13 21:26:44 +0100

    Add files via upload
    deleted unnecessary comments

--- storage_weighing.ino
+++ storage_weighing.ino
@@ -17,21 +17,22 @@ const int wake_up = 6;                // do driveru vzbuzeni
 const int tenzometr_DT = 7;           // do ADC comm
 const int tenzometr_SCK = 8;          // do ADC comm
 const int measure_start = 9;          // tlacitko povel mereni
-const int tare_btn = 10;              // tlacitko povel tare
+const int tare_btn = 10;              // tlacitko povel tare a stop otaceni na 10 s
 
 long angle = 0;                       // aktualni poloha prepocet "poloha"
 int desired_angle = 0;
-int slot[4] = {32, 121, 212, 300}; // ori uhly 34 124 214 305 uhly zmeneny kvuli pólům motoru a jejich magnetizaci ve stavu bez napětí
+int slot[4] = {32, 121, 212, 300}; // orig uhly 34 124 214 305 uhly zmeneny kvuli pólům motoru a jejich magnetizaci ve stavu bez napětí
 //bool full_slot[4] = {0, 0, 0, 0};
 float base_A;                         // dobry by bylo zahodit floaty i double
 float base_B;                         // weight od base to be substracted from measured amount
-float base_weight_A;                         // dobry by bylo zahodit floaty i double
+float base_weight_A;                  // dobry by bylo zahodit floaty i double
 float base_weight_B;  
-int num_prijem = 0;
-int slot_i = 0;
+int num_prijem = 0;                   // cislo prikazu
+int slot_i = 0;                       // aktivni slot
+float max_weiht = 1200;
 
 struct output_data{
-  uint16_t data_out = 0;     // weight
+  uint16_t data_out = 0;              // weight
   uint8_t message = B00000000;
   // XXXX XXXX binary means XX00 0000 = active slot, 
   //                        00X0 0000 = scale tared, 
@@ -48,8 +49,7 @@ struct inner_data{
   bool pos_not_found = 0;
   bool unknown_command = 0;
   bool magasine_stuck = 0;
-  bool comm_fail = 0;
-  bool sound_of_silence = 0;      // 0 = komunikace poslouchá; 1 = neposlouchá
+  bool sound_of_silence = 0;      // 0 = komunikace poslouchá; 1 = neposlouchá; NEAKTIVNI
   bool weight_overload = 0;
   uint8_t time = 0;
   };
@@ -74,20 +74,17 @@ void setup() {
   scale.begin(tenzometr_DT, tenzometr_SCK);   // innit dvojkanálového ADC
   scale.set_gain(128); //128                        // tare tenzometru pro zesileni 128
   base_A = scale.read_average(5);
-  base_weight_A = (1000-0)*((base_A)/(16777216.0));        // chat approved
+
   scale.set_gain(32);                         // tare tenzometru pro zesileni 32
   base_B = scale.read_average(5);
-  base_weight_B = (1000-0)*((base_B)/(16777216.0));        // chat approved
   
-  //desired_angle = 0;
   angle = (360.0 / 675.0) * analogRead(hall_senz);        // 675 je max hodnota vstupu 3,3v do adc prevodniku arduina
   //desired_angle = angle;
   move(slot[slot_i], 3840);
   Serial.println("slot 0 aktivni, program pripraven na funkci");
   Serial.print("adc hodnota hall senzoru: \t\t");
   Serial.println(analogRead(hall_senz));
-  Serial.println("Version 12m 22d");
-//  digitalWrite(DIR, HIGH);                  // default smer jízdy
+  Serial.println("Version 2m 13d");
 }
 
 void loop() {
@@ -103,7 +100,15 @@ void loop() {
       slot_i = slot_i + 1;
     }
     if(move(slot[slot_i], 2500) < 0){               // fnc pohyb na zadaný úhel, timeout ma byt 1920
-      Serial.println("nebyl dosazen zadany slot");
+      if(slot_i <= 0){
+        slot_i = 3;
+      }
+      else {
+        slot_i = slot_i - 1;
+      }
+      Serial.print("nebyl dosazen zadany slot, aktivni slot: ");
+      Serial.println(slot_i);
+      inner.manual_mode = 0;
     }
     else {
       Serial.print("aktivni slot");
@@ -120,7 +125,6 @@ void loop() {
   else if(digitalRead(tare_btn) == LOW){                 // tare tenzometru (vynulovani)
     inner.manual_mode = 1;
     Serial.println("Hold and Tare button pushed");
-    //Serial.println("Tare button pushed");
     hold_10sec();
     tare_bases();
     inner.manual_mode = 0;
@@ -133,17 +137,10 @@ void loop() {
     Serial.print("pohnuto hrideli -> uprava; aktualni uhel: ");
     Serial.println(angle);
     move(desired_angle, 3840);                                   // melo by byl 1920
-    //hold_10sec();                                              // later erase, je to jen test drzeni motoru na miste 5s
     //inner.time = 0;
     //}
   }
-  /*
-  if(num_prijem == 0 && (desired_angle != angle && abs(desired_angle - angle) > 2)){  // hold the line, pro odchylku > 2 se pohni
-    Serial.println("pohnuto hrideli -> uprava");
-    move(desired_angle, 1920);
-    //hold_10sec();                                                // later erase, je to jen test drzeni motoru na miste 5s
-  }
-*/
+
   if(num_prijem != 0){                        // deal with communication
     if(deal_w_comm() < 0){
       Serial.println("Communication unresolved");
@@ -160,13 +157,12 @@ void loop() {
 void receive_data(int numBytes) {         // detekce a vypsani prichozi komunikace
   if(inner.manual_mode == 0){ //inner.sound_of_silence == 0
     inner.sound_of_silence = 1;
-    //while (Wire.available()) {
     num_prijem = Wire.read();
-    //}
     Serial.print("Received data: ");
     Serial.println(num_prijem);
   }
 }
+
 void send_data() {                        // odeslani vyzadanych dat
   const uint8_t out_arr_size = 3;
   uint8_t output_array[out_arr_size] = {0};
@@ -211,7 +207,6 @@ int deal_w_comm(){
       case 10:
         tare_bases();
         inner.scale_tared = 1;                            // scale ready report
-        //previous line out
         break;
       case 20:
         output.data_out = measure();
@@ -267,7 +262,7 @@ int deal_w_comm(){
   return 1;
 }
 int move(int chci_sem, int timeout){           // Imax = 400mA; timeout 120 kroku max -> 1/4 pomer = 480, 1/8 pomer = 960, 1/16 pomer = 1920, 1/32 pomer = 3840;
-  //timeout = 100;
+  //timeout = 100;            // overwrite zadaneho timeoutu
   int po_smeru = chci_sem - angle;
   if(po_smeru < 0){
     po_smeru = 360 + po_smeru;
@@ -306,7 +301,8 @@ int move(int chci_sem, int timeout){           // Imax = 400mA; timeout 120 krok
     }
     else{
       angle = chci_sem;                                // ends while cycle
-      inner.pos_not_found = 1;                         // desired position not found
+      //inner.pos_not_found = 1;                         // desired position not found
+      inner.magasine_stuck = 1;
       return -1;
     }
   }
@@ -320,11 +316,9 @@ int move(int chci_sem, int timeout){           // Imax = 400mA; timeout 120 krok
 double measure(){                                        // mereni vahy (weight_A a weight_B)
   double real_weight_A;
   double real_weight_B;
-  double max_val = 8377608;                        //16777216 , 8377608
+  //double max_val = 8377608;                        //16777216 , 8377608
   scale.power_up();                                    // huh, you are finally awake
   scale.set_gain(128);//128                                 // nastaveni aktivni kanal A zesileni 128
-
-  double Weight_per_bit = //1000/max_val;
 
   Serial.println("KANAL A");
 
@@ -334,9 +328,8 @@ double measure(){                                        // mereni vahy (weight_
   Serial.print("read - base: \t\t");
   Serial.println(tmp_vaha - base_A);
 
-  real_weight_A = (tmp_vaha - base_A)/1605,51;                // 401,3775 je narust hodnoty ADC pri zmene o 0,5g
+  //real_weight_A = (tmp_vaha - base_A)/1605,51;                // 401,3775 je narust hodnoty ADC pri zmene o 0,5g OLD
   real_weight_A = (tmp_vaha - base_A - 1,78)/1605,99602;        // vchazi z lin. rov. y=823,71x+1312,5                  NETESTOVANO
-          // 0,97 je systematická odchylka idk jestli provizorní konstrukcí nebo zlým měřítkem
   Serial.print("weight in grams: \t\t");
   Serial.println(real_weight_A);
 
@@ -351,7 +344,7 @@ double measure(){                                        // mereni vahy (weight_
   Serial.print("read - base: \t\t");
   Serial.println(tmp_vaha - base_B);
 
-  real_weight_B = (tmp_vaha - base_B)/768,5;                  // 192,125 je narust hodnoty ADC pri zmene o 0,5g
+  //real_weight_B = (tmp_vaha - base_B)/768,5;                  // 192,125 je narust hodnoty ADC pri zmene o 0,5g   OLD
   real_weight_B = (tmp_vaha - base_B + 1,39)/768,00273;        // vychzi z lin. rov. y=383,23x+1155,5                NETESTOVANO
   Serial.print("weight in grams: \t\t");
   Serial.println(real_weight_B);
@@ -361,13 +354,9 @@ double measure(){                                        // mereni vahy (weight_
   Serial.print("final weight: \t\t");
   Serial.println(real_weight_B + real_weight_A); //real_weight_B - base_weight_B + real_weight_A - base_weight_A
 
-  //float sum_baze = Base_A + 4 * Base_B;
-  //float real_weight_suma = (2000-0) * ((tmp_vaha-sum_baze)/(2*16777216 - sum_baze));
-  //Serial.print("suma vahy: ")
   if(real_weight_A + real_weight_B > 1200){         // váha báze je 700g -> 1200g mohu měřit a zbyde mi 100g jako rezerva overloadu
     inner.weight_overload = 1;
   }
-  //Serial.println(long(real_weight_A + real_weight_B));    // pokus konverze
   return real_weight_B + real_weight_A;
 }
 void tare_bases(){                 //  later erase, or built to operator panel
