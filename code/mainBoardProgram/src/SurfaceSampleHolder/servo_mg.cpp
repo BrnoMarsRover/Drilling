413e8cde55b468dad5dfe4d762ab8a8626cf51d5
Spejb1 <160340105+Spejb1@users.noreply.github.com>
2026-05-27 17:18:33 +0200

    servo lib and implementation finished not tested

--- code/mainBoardProgram/src/SurfaceSampleHolder/servo_mg.cpp
+++ code/mainBoardProgram/src/SurfaceSampleHolder/servo_mg.cpp
@@ -1,49 +1,52 @@
 #include "SERVO_MG.h"
 
 SERVO_MG::SERVO_MG(int pin)
-    : _pin(pin),
-      _currentPos(CLOSED_ANGLE),
-      _targetPos(CLOSED_ANGLE),
-      _lastStepTime(0)
+  : _pin(pin),
+    _currentPos(CLOSED_ANGLE),
+    _targetPos(CLOSED_ANGLE),
+    _lastStepTime(0)
 {}
 
-void SERVO_MG::begin() {
-    _servo.setPeriodHertz(50);
-    _servo.attach(_pin, 1000, 2000);
-    _servo.write(_currentPos);
+bool SERVO_MG::begin() {
+  _servo.setPeriodHertz(50);
+  _servo.attach(_pin, 1000, 2000);
+  _servo.write(_currentPos);
+  return true;
 }
 
-void SERVO_MG::openRockBox() {
-    setPos(OPEN_ANGLE);
+bool SERVO_MG::openBox() {
+  return setPos(OPEN_ANGLE);
 }
 
-void SERVO_MG::closeRockBox() {
-    setPos(CLOSED_ANGLE);
+bool SERVO_MG::closeBox() {
+  return setPos(CLOSED_ANGLE);
 }
 
-void SERVO_MG::setPos(uint16_t angle) {
-    if (angle > 180) angle = 180;
-    if ((int)angle != _currentPos) {
-        _targetPos = (int)angle;
-    }
+bool SERVO_MG::setPos(uint8_t angle) {
+  if (angle > 180) 
+    angle = 180;
+  if (angle != _currentPos) {
+    _targetPos = angle;
+  }
+  return true;
 }
 
-uint16_t SERVO_MG::getPos() {
-    return (uint16_t)_currentPos;
+uint8_t SERVO_MG::getPos() {
+  return (uint8_t)_currentPos;
 }
 
 void SERVO_MG::update() {
-    unsigned long now = millis();
-    if (now - _lastStepTime < STEP_DELAY_MS) return;
-    _lastStepTime = now;
+  unsigned long now = millis();
+  if (now - _lastStepTime < STEP_DELAY_MS) return;
+  _lastStepTime = now;
 
-    if (_currentPos < _targetPos) {
-        _currentPos++;
-    } else if (_currentPos > _targetPos) {
-        _currentPos--;
-    } else {
-        return;
-    }
+  if (_currentPos < _targetPos) {
+      _currentPos++;
+  } else if (_currentPos > _targetPos) {
+      _currentPos--;
+  } else {
+      return;
+  }
 
-    _servo.write(_currentPos);
+  _servo.write(_currentPos);
 }
